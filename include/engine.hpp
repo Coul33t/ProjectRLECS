6aead7bedfa53df5db283515899b61fe2e27124d
Quentin Couland <Coulis1990@gmail.com>
2022-01-23 19:45:43 +0100

    :fire: Removed unused method signature

--- include/engine.hpp
+++ include/engine.hpp
@@ -22,7 +22,6 @@ class Engine {
         void initSystems();
         void initRenderer(uint w = 80, uint h = 50);
         void initMap(uint w = 40, uint h = 20);
-        void initPlayer();
 
         void populateMap();
 
