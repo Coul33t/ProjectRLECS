db5ffd7402a6c0fbc6fcf55577c564f0feb09f73
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-01-24 16:39:42 +0100

    :question: Minor

--- include/entities_factory.hpp
+++ include/entities_factory.hpp
@@ -3,6 +3,7 @@
 #include "components/component.hpp"
 #include "tags/tags.hpp"
 #include "../extlib/flecs/flecs.h"
+#include "colours.hpp"
 
 namespace EntFactories {
     flecs::entity createPlayer(flecs::world& ecs_world, uint x, uint y);
