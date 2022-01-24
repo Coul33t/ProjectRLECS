98906d47a6517ce1e17673a6d7c6eaed24d60ce7
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-01-24 16:35:28 +0100

    :art: New colours

--- include/colours.hpp
+++ include/colours.hpp
@@ -1,3 +1,6 @@
+#pragma once
+#include "libtcod_no_warnings.hpp"
+
 static const TCODColor default_bg(0, 0, 0);
 
 static const TCODColor light_wall(130, 110, 50);
@@ -7,4 +10,7 @@ static const TCODColor dark_wall(32, 22, 12);
 static const TCODColor dark_ground(50, 45, 12);
 
 static const TCODColor green(0, 255, 0);
+static const TCODColor dark_green(0, 100, 0);
+
 static const TCODColor red(255, 0, 0);
+static const TCODColor dark_red(100, 0, 0);
