8e5a88f166a4961a1b8317a3345040fac7341ff3
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-01-31 12:01:39 +0100

    :sparkles: Added GUIs

--- include/gui.hpp
+++ include/gui.hpp
@@ -1,21 +1,92 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include "colours.hpp"
 #include "types.hpp"
 #include "libtcod_no_warnings.hpp"
+
+struct ConTile {
+    char chr;
+    TCODColor bg, fg;
+
+    ConTile() {
+        chr = ' ';
+        bg = black;
+        fg = white;
+    }
+};
+
+struct Message {
+    uint x, y;
+    std::string content;
+    TCODColor fg, bg;
+    
+    Message() {
+        x = 0;
+        y = 0;
+        content = "MESSAGE CONTENT NOT SET";
+        fg = TCODColor(255, 255, 255);
+        bg = TCODColor(  0,   0,   0);
+    };
+
+    Message(uint x, uint y, std::string content): x(x), y(y), content(content) {
+        fg = TCODColor(255, 255, 255);
+        bg = TCODColor(  0,   0,   0);
+    };
+
+    Message(uint x, uint y, std::string content, TCODColor& fg, TCODColor& bg):
+        x(x), y(y), content(content), fg(fg), bg(bg) {};
+};
+
+struct Bar {
+    uint x, y, width;
+    std::string name;
+    int val;
+    uint max_val;
+    TCODColor fg, bg, msg_fg;
+
+    Bar() {
+        x = 0;
+        y = 0;
+        width = 5;
+        name = "NO NAME SET";
+        val = 5;
+        max_val = 10;
+        fg =     TCODColor(255, 255, 255);
+        bg =     TCODColor(  0,   0,   0);
+        msg_fg = TCODColor(125, 125, 125);
+    }
+
+    Bar(uint x, uint y, uint width, std::string name, int val, 
+        uint max_val, TCODColor& fg, TCODColor& bg, TCODColor& msg_fg):
+            x(x), y(y), width(width), name(name), val(val),
+            max_val(max_val), fg(fg), bg(bg), msg_fg(msg_fg) {};
+};
+
+
 class Gui {
     public:
         Gui();
-        Gui(uint x, uint y, uint width, uint height);
+        Gui(uint x, uint y, uint width, uint height, std::string name);
         ~Gui();
 
-        void renderBar(uint x, uint y, uint width, std::string name, int val, 
-                       uint max_val, TCODColor& colour, TCODColor& back_colour);
+        void setGuiAttributes(std::string name, uint x, uint y, uint w, uint h);
+
+        void addBar(uint x, uint y, uint width, std::string name, int val, 
+                    uint max_val, TCODColor& fg, TCODColor& bg, TCODColor& msg_fg);
+
+        void updateBarPos(std::string bar_name, uint new_x, uint new_y);
+        void updateBarVal(std::string bar_name, uint new_val, uint new_max_val);
+        void updateBarColours(std::string bar_name, TCODColor& new_fg, TCODColor& new_bg, TCODColor& new_msg_fg);
+
+        void renderBars();
         
         void render(bool debug=false);
 
         // No choice or else weird error about being protected
+        std::string name;
         TCODConsole* con;
-        TCODColor debug_colour;
         mVec2<uint> pos, size;
+        std::vector<Bar> bars;
+        std::vector<Message> messages;
 };
--- src/gui.cpp
+++ src/gui.cpp
@@ -6,28 +6,93 @@ Gui::Gui() {
     size.w = 0;
     size.h = 0;
     con = new TCODConsole(size.w, size.h);
-    //debug_colour = TCODColor(255, 0, 0);
+    name = "GUI NAME NOT SET";
 }
 
-Gui::Gui(uint x, uint y, uint width, uint height) {
+Gui::Gui(uint x, uint y, uint width, uint height, std::string name) {
     pos.x = x;
     pos.y = y;
     size.w = width;
     size.h = height;
     con = new TCODConsole(width, height);
+    name = name;
 }
 
 Gui::~Gui() {
     delete con;
 }
 
-void Gui::renderBar(uint x, uint y, uint width, std::string name, int val, 
-                     uint max_val, TCODColor& colour, TCODColor& back_colour) {
+void Gui::setGuiAttributes(std::string name, uint x, uint y, uint w, uint h) {
+    this->name = name;
+    pos.x = x;
+    pos.y = y;
+    size.w = w;
+    size.h = h;
+}
 
+void Gui::addBar(uint x, uint y, uint width, std::string name, int val, 
+                 uint max_val, TCODColor& fg, TCODColor& bg, TCODColor& msg_fg) {
+    bars.push_back(Bar(x, y, width, name, val, max_val, fg, bg, msg_fg));
+}
+
+void Gui::updateBarPos(std::string bar_name, uint new_x, uint new_y) {
+    for (auto it = bars.begin(); it != bars.end(); it++) {
+        if ((*it).name == bar_name) {
+            (*it).x = new_x;
+            (*it).y = new_y;
+        }
+    }
+}
+
+void Gui::updateBarVal(std::string bar_name, uint new_val, uint new_max_val) {
+    for (auto it = bars.begin(); it != bars.end(); it++) {
+        if ((*it).name == bar_name) {
+            (*it).max_val = new_max_val;
+            (*it).val = new_val;
+        }
+    }
+}
+
+void Gui::updateBarColours(std::string bar_name, TCODColor& new_fg, TCODColor& new_bg, TCODColor& new_msg_fg) {
+    for (auto it = bars.begin(); it != bars.end(); it++) {
+        if ((*it).name == bar_name) {
+            (*it).fg = new_fg;
+            (*it).bg = new_bg;
+            (*it).msg_fg = new_msg_fg;
+        }
+    }
+}
+
+void Gui::renderBars() {
+    for (auto it_bar = bars.begin(); it_bar != bars.end(); it_bar++) {
+        con->setDefaultBackground((*it_bar).bg);
+        con->rect((*it_bar).x, (*it_bar).y, (*it_bar).width, 1, false, TCOD_BKGND_SET);
+        
+        int fill_width = (int)((*it_bar).val / (*it_bar).max_val * (*it_bar).width);
+        
+        if (fill_width > 0) {
+            con->setDefaultBackground((*it_bar).fg);
+            con->rect((*it_bar).x, (*it_bar).y, fill_width, 1, false, TCOD_BKGND_SET);
+        }
+
+        con->setDefaultBackground((*it_bar).msg_fg);
+        con->printEx((*it_bar).x + (*it_bar).width / 2, (*it_bar).y, TCOD_BKGND_NONE, TCOD_CENTER,
+            "%s: %g/%g", (*it_bar).name, (*it_bar).val, (*it_bar).max_val);
+    }
 }
 
 void Gui::render(bool debug) {
-    if(debug) {
+    con->clear();
 
+    if(debug) {
+        con->setDefaultBackground(red);
+        con->rect(0, 0, size.w, size.h, false, TCOD_BKGND_SET);
     }
+
+    else {
+        renderBars();
+    }
+
+    TCODConsole::blit(con, 0, 0, size.w, size.h, 
+                      TCODConsole::root, pos.x, pos.y);
 }
