3625cfc20dea12aee952e94a88667ded88fd03b8
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-03-23 11:11:32 +0100

    :sparkles: :bug: Changed colours and way to display bars

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -28,6 +28,12 @@ void Renderer::initConsole(uint w, uint h) {
     terminal_color("black");
 }
 
+void Renderer::initPlayerBars(flecs::world& ecs_world) {
+    auto stats = ecs_world.lookup("Player").get<Stats>();
+    //uint x, uint y, uint width, std::string name, int val, uint max_val, color_t fg, color_t bg, color_t msg_fg
+    gui_stats.addBar(1, 1, 10, "HP", stats->hp - 10, stats->max_hp - 5, Colours::red, Colours::dark_red, Colours::black);
+}
+
 void Renderer::setConsole(uint w, uint h) {
     std::string size = "window: size=" + std::to_string(w) + "x" + std::to_string(h);
     terminal_set(size.c_str());
@@ -48,31 +54,31 @@ void Renderer::renderString(uint x, uint y, std::string str, color_t fg, color_t
 void Renderer::renderTile(Map& map, uint x, uint y, bool debug) {
     if (debug) {
         if (map.isWalkable(x, y))
-            renderChar(x, y, '.', light_ground, default_bg);
+            renderChar(x, y, '.', Colours::light_ground, Colours::default_bg);
         else
-            renderChar(x, y, '#', light_wall, default_bg);
+            renderChar(x, y, '#', Colours::light_wall, Colours::default_bg);
     }
 
     else {
         if(map.isInFov(x, y)) {   
             if (map.isWalkable(x, y))
-                renderChar(x, y, '.', light_ground, default_bg);
+                renderChar(x, y, '.', Colours::light_ground, Colours::default_bg);
             else
-                renderChar(x, y, '#', light_wall, default_bg);
+                renderChar(x, y, '#', Colours::light_wall, Colours::default_bg);
         }
 
         else if(map.isExplored(x, y)) {
             if (map.isWalkable(x, y))
-                renderChar(x, y, '.', dark_ground, default_bg);
+                renderChar(x, y, '.', Colours::dark_ground, Colours::default_bg);
             else
-                renderChar(x, y, '#', dark_wall, default_bg);
+                renderChar(x, y, '#', Colours::dark_wall, Colours::default_bg);
 
         }
     }
 }
 
 void Renderer::renderMap(Map& map, bool debug) {
-    terminal_bkcolor(black);
+    terminal_bkcolor(Colours::black);
 
     for(size_t y = 0; y < map.size.h; y++) {
         for (size_t x = 0; x < map.size.w; x++) {
@@ -85,7 +91,7 @@ void Renderer::renderEntities(Map& map, flecs::world& ecs_world) {
     // Draw the dead entities first (so they are under everything else)
     ecs_world.each([&map, this](flecs::entity e, const Position& p, const Renderable& r, const Dead& d) {
         if(map.isInFov(p.x, p.y)) {
-            renderChar(p.x, p.y, r.glyph_dead, r.colour_dead, default_bg);
+            renderChar(p.x, p.y, r.glyph_dead, r.colour_dead, Colours::default_bg);
         }
           
     });
@@ -94,7 +100,7 @@ void Renderer::renderEntities(Map& map, flecs::world& ecs_world) {
     ecs_world.each([&map, this](flecs::entity e, const Position& p, const Renderable& r, const Alive& a) {
         if(!e.has<Player>()) {
             if(map.isInFov(p.x, p.y)) {
-                renderChar(p.x, p.y, r.glyph, r.colour, default_bg);
+                renderChar(p.x, p.y, r.glyph, r.colour, Colours::default_bg);
             }
         }  
     });
@@ -106,32 +112,38 @@ void Renderer::renderEntities(Map& map, flecs::world& ecs_world) {
     flecs::entity player = ecs_world.lookup("Player");
     const Position* p = player.get<Position>();
     const Renderable* r = player.get<Renderable>();
-    renderChar(p->x, p->y, r->glyph, r->colour, default_bg);
+    renderChar(p->x, p->y, r->glyph, r->colour, Colours::default_bg);
 }
 
-void Renderer::renderStats() {
+void Renderer::renderStats(flecs::world& ecs_world) {
+    auto stats = ecs_world.lookup("Player").get<Stats>();
+    auto melee = ecs_world.lookup("Player").get<Melee>();
+
     // Render the bars
-    /*for (auto it_bar = gui_stats.bars.begin(); it_bar != gui_stats.bars.end(); it_bar++) {
-        std::string bg_bar = ((*it_bar).width, " ");
+    for (auto it_bar = gui_stats.bars.begin(); it_bar != gui_stats.bars.end(); it_bar++) {
+        std::string bg_bar = std::string((*it_bar).width, ' ');
         renderString((*it_bar).x + gui_stats.pos.x, (*it_bar).y + gui_stats.pos.y, bg_bar, (*it_bar).bg, (*it_bar).bg);     
         
-        int fill_width = (int)((*it_bar).val / (*it_bar).max_val * (*it_bar).width);
+        int fill_width = (int)((float)(*it_bar).val / (float)(*it_bar).max_val * (float)(*it_bar).width);
 
-        std::string fg_bar = (fill_width, " ");
-        renderString((*it_bar).x + gui_stats.pos.x, (*it_bar).y + gui_stats.pos.y, fg_bar, (*it_bar).bg, (*it_bar).bg);     
-    }*/
+        std::string fg_bar = std::string(fill_width, ' ');
+        renderString((*it_bar).x + gui_stats.pos.x, (*it_bar).y + gui_stats.pos.y, fg_bar, (*it_bar).fg, (*it_bar).fg);     
+    }
 
-    for (size_t y = gui_stats.pos.y; y < gui_stats.pos.y + gui_stats.size.h; y++) {
+    gui_stats.renderString(1, 2, std::string("Melee dmg: " + std::to_string(melee->dmg)), Colours::white, Colours::default_bg);
+
+    // Debug
+    /*for (size_t y = gui_stats.pos.y; y < gui_stats.pos.y + gui_stats.size.h; y++) {
         for (size_t x = gui_stats.pos.x; x < gui_stats.pos.x + gui_stats.size.w; x++) {
             renderChar(x, y, 'X', red, white);
         }
-    }
+    }*/
 }
 
 void Renderer::renderMessages() {
     for (size_t y = gui_messages.pos.y; y < gui_messages.pos.y + gui_messages.size.h; y++) {
         for (size_t x = gui_messages.pos.x; x < gui_messages.pos.x + gui_messages.size.w; x++) {
-            renderChar(x, y, 'X', green, white);
+            renderChar(x, y, 'X', Colours::green, Colours::white);
         }
     }
 }
@@ -139,13 +151,13 @@ void Renderer::renderMessages() {
 void Renderer::renderInfo() {
     for (size_t y = gui_info.pos.y; y < gui_info.pos.y + gui_info.size.h; y++) {
         for (size_t x = gui_info.pos.x; x < gui_info.pos.x + gui_info.size.w; x++) {
-            renderChar(x, y, 'X', blue, white);
+            renderChar(x, y, 'X', Colours::blue, Colours::white);
         }
     }
 }
 
-void Renderer::renderGUIs() {
-    renderStats();
+void Renderer::renderGUIs(flecs::world& ecs_world) {
+    renderStats(ecs_world);
     renderMessages();
     renderInfo();
     /*TCODConsole::blit(gui_messages.con, 0, 0, gui_messages.size.w, gui_messages.size.h, 
